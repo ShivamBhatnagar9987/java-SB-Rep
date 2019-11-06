4124f915664583a21e7f966acfb9a0c200aa91db
ShivamBhatnagar9987 <shivambhatnagar94@icloud.com>
2019-11-06 15:27:47 +0530

    Course API getting topic by Id and adding new Topic instance
    changes done: A new topic is sent from a header body and is then added to our topic list moreover a funictionality of topic retrieval by id has also been introduced

--- course-api/src/main/java/com/bhatnagar/springBootStarter/topic/TopicController.java
+++ course-api/src/main/java/com/bhatnagar/springBootStarter/topic/TopicController.java
@@ -3,7 +3,10 @@ package com.bhatnagar.springBootStarter.topic;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,4 +19,15 @@ public class TopicController {
 	public List<Topic> getAllTopics() {
 		return topicService.getAllTopics();
 	}
+
+	@RequestMapping("/topics/{id}")
+	public Topic getTopicById(@PathVariable String id) {
+		return topicService.getTopicById(id);
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+	}
+
 }
--- course-api/src/main/java/com/bhatnagar/springBootStarter/topic/TopicService.java
+++ course-api/src/main/java/com/bhatnagar/springBootStarter/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.bhatnagar.springBootStarter.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,13 +8,22 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
-	private List<Topic> topics = Arrays.asList(new Topic("spring", "Spring Framework", "description"),
-			new Topic("java", "core java", "core java description"),
-			new Topic("javascript", "javascript", "javascript description")
+	private List<Topic> topics = new ArrayList<Topic>(
+			Arrays.asList(new Topic("spring", "Spring Framework", "description"),
+					new Topic("java", "core java", "core java description"),
+					new Topic("javascript", "javascript", "javascript description")
 
-	);
+			));
 
 	public List<Topic> getAllTopics() {
 		return topics;
+	}
+
+	public Topic getTopicById(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
 	}
 }
